73fcfba8148d2dd184f7fb72989861cf3898e09c
Sergey Grigorchuk <sng@cynteka.ru>
2018-05-01 13:47:22 +0300

    Webhook can be specified only in web hook profile mode

--- src/main/java/com/wyc/viber/AutoStart.java
+++ src/main/java/com/wyc/viber/AutoStart.java
@@ -6,11 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.wyc.WYCConfig;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 public class AutoStart {
+
+	@Autowired
+	private WYCConfig wycConfig;
 
 	@Autowired
 	private ViberBot viberBot;
@@ -20,7 +25,7 @@ public class AutoStart {
 		// Стартовать сразу нельзя- потому что сразу после операции set_webhook viber пойдет его проверять,
 		// а порт на прослушку еще не открыт- поэтому надо подождать какое-то время
 		try {
-			if(!viberBot.isInitialized()) {
+			if(!viberBot.isInitialized() && wycConfig.isWebHook()) {
 				viberBot.initialize();
 			}
 		} catch (IOException e) {
